00876bad20a0f48491d2bc7fbb41adbcd53a0b59
sanabyte2 <68213476+sanabyte2@users.noreply.github.com>
2020-08-03 06:30:07 -0700

    Update romina-tictactoefinal.java
    edited last saved copy of this file: organized main method body into separate functions for more clarity, fixed bugs (user is continuously prompted until they enter appropriate row and column number within specified range, user cannot take spaces which are taken and will be prompted to choose another row and column number)

--- romina-tictactoefinal.java
+++ romina-tictactoefinal.java
@@ -1,91 +1,76 @@
 import java.util.*;
-import java.util.Scanner
+import java.util.Scanner;
 public class TicTacToe {
 	
 static boolean hasWon = false;
 static char[][] board = new char[3][3];
+Scanner s = new Scanner (System.in);
 
-int row
-int column
-int row2
-int column2;
+static int row = 0;
+static int column = 0;
+static int row2 = 0;
+static int column2 = 0;
 	
-public static void main (String [] args){
-Scanner s = new Scanner (System.in); 
-		
+public static void main (String [] args){ 
+		TicTacToe myClassObject = new TicTacToe();
 		System.out.println("Welcome to Tic Tac Toe! You are player X, and player O is your opponent.");
 		System.out.println("Please ensure two people are playing.");
-		System.out.println("Player X, is it your turn!");
+	
 	while (!hasWon){
-		do {
-			System.out.println("Enter a row number from (0-2)");
-			row = s.nextInt();
-		} while ((row < 0 || row > 2));
-		System.out.println("Entered row number is within specified parameters.");
-		do {
-			System.out.println("Enter a row number from (0-2)");
-			column = s.nextInt();
-		} while ((column < 0 || column > 2));
-		System.out.println("Entered column number is within specified parameters.");
-		
-		//if-else block below does not work to 1. ensure player is not overriding taken space 2. re-prompt player to choose a new column/row #
-		if(board[row][column] != ' ') {
-			board[row][column] = 'O';
-		} else { 
-			System.out.println("This position is taken! Please enter a new row and column number.");
-			
-		/* @Romina, how does the following work to accomplish the above goals? 
-		if (board[row][column] == 0){
-			board[row][column] = 'X';
-		} 
-		else if (board[row][column] != 0){
-			System.out.println("ERROR DETECTED: You may not pick this row and column as it is taken.");
-		}
-		*/
-		
+		myClassObject.inputPlayerX();
+		myClassObject.printBoard(board);
 		checkWin();
-		System.out.println(" " + board[0][0] + " |" + " " + board[0][1] + " |" +  " " + board[0][2] + " ");
-		System.out.println("- - - - - -");
-		System.out.println(" " + board[1][0] + " |" + " " + board[1][1] + " |" + " " + board[1][2] + " ");
-		System.out.println("- - - - - -");
-		System.out.println(" " + board[2][0] + " |" + " " + board[2][1] + " |" +  " " + board[2][2] + " ");
-			
-		
-		
-		System.out.println("Player O, it is now your turn!");
-		do { 
-			System.out.println("Enter a row number from (0-2)");
-			row2 = s.nextInt();
-		} while ((row2 < 0 || row2 > 2));
-		System.out.println("Entered row number is within specified parameters.");
-		do {
-			System.out.println("Enter a column number from (0-2)");
-			column2 = s.nextInt();
-		} while ((column2 < 0 || column2 > 2));
-		System.out.println("Entered column number is within specified parameters.");
-		
-		//if-else block below does not work to 1. ensure player is not overriding taken space 2. re-prompt player to choose a new column/row #
-            	if ( (board[row2][column2] != ' ') || (board[row][column] != ' ') ) {
-                	board[row2][column2] = 'X';
-            	} else {
-                System.out.println("This board position it is already taken! Please enter a new row and a new column number.");
-            	}
-		
-		/* @Romina, how does the following work to accomplish the above goals? 
-		board[row2][column2] = 'O';
-		if (board[row][column] != 0){
-		System.out.println("ERROR DETECTED: You may not pick this row and column as it is taken.");
-		*/ 
-			
+		myClassObject.checkValidForPlayer0(row2, column2);
 		checkWin();
-		System.out.println(" " + board[0][0] + " |" + " " + board[0][1] + " |" +  " " + board[0][2] + " ");
-		System.out.println("- - - - - -");
-		System.out.println(" " + board[1][0] + " |" + " " + board[1][1] + " |" + " " + board[1][2] + " ");
-		System.out.println("- - - - - -");
-		System.out.println(" " + board[2][0] + " |" + " " + board[2][1] + " |" +  " " + board[2][2] + " ");
 	}
 }
 
+public void printBoard(char[][] board) {
+	System.out.println(" " + board[0][0] + " |" + " " + board[0][1] + " |" +  " " + board[0][2] + " ");
+		System.out.println("- - - - - -");
+		System.out.println(" " + board[1][0] + " |" + " " + board[1][1] + " |" + " " + board[1][2] + " ");
+		System.out.println("- - - - - -");
+		System.out.println(" " + board[2][0] + " |" + " " + board[2][1] + " |" +  " " + board[2][2] + " ");
+}
+
+public void inputPlayer() {
+	do {
+	   System.out.println("Player X: Enter a row number from (0-2)");
+	   row = s.nextInt();
+	   System.out.println("Player X: Enter a column number from (0-2)");
+	   column = s.nextInt();
+	  } while ((row < 0 || row > 2) && (column < 0 || column > 2));
+	  checkValidForPlayerX(row, column);
+}
+
+public void inputPlayer0() {
+	do {
+	   System.out.println("Player O: Enter a row number from (0-2)");
+	   row2 = s.nextInt();
+	   System.out.println("Player O: Enter a column number from (0-2)");
+	   column2 = s.nextInt();
+	  } while ((row2 < 0 || row2 > 2) && (column2 < 0 || column2 > 2));
+	  checkValidForPlayerO(row2, column2);
+}
+
+public void checkValidForPlayerX(int row, int column) {
+	if(board[row][column] == ' ' || board[row][column] == 'X' || board[row][column] == 'O')) {
+		System.out.println("Player X: Your number entries are valid!");
+		board[row][column] == 'X';
+	} else {
+		inputPlayerX();
+	}
+}
+
+publicvoid checkValidForPlayerO(int row2, int column2) {
+	if(!(board[row2][column2] == ' ') || board[row][column] == 'X' || board[row2][column2] == '0')){
+		System.out.println("Player O: Your row and column numbers are valid!");
+		board[row2][column2] = 'O';
+	} else {
+		inputPlayerO();
+	}
+}
+	
 static void checkWin() {
 	//across top
 			if (board[0][0] == board [0][1] && board[0][0] == board[0][2] && board[0][0] != 0){
@@ -130,5 +115,5 @@ static void checkWin() {
 			if (board[0][0] != 0 && board[0][1] != 0 && board[0][2] != 0 && board[1][0] != 0 && board[1][1] != 0 && board[1][2] != 0 && board[2][0] != 0 && board[2][1] != 0 && board[2][2] != 0){
 				System.out.println("Game over; no winner could be determined.");
 			}
-}
+	}
 }
